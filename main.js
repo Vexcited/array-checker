773c7d294e8f07aa8043cc768aa8b16e7a2c0576
Vexcited <vexitofficial@gmail.com>
2020-12-13 18:49:22 +0100

    CodeMirror sur les `textarea`s ; Ajout de plusieurs modèles ; Modifications de la page + Bulma Modifs ; Couleurs Nord Theme améliorés

--- main.js
+++ main.js
@@ -34,146 +34,156 @@ let deleteSvg = `
     </svg>
 `;
 
-// Initialisation des valeurs
-let objectOfArrays = "", arrayToCompare = {};
-const objectOfArraysInput = get('#objectOfArraysInput');
+// Initialisation des variables
+let arrayListes = "", arrayModeles = "";
 
-// Live check onInput
-objectOfArraysInput.addEventListener("input", function (el) {
-    objectOfArrays = this.value;
-    check(objectOfArrays, arrayToCompare);
-});
+// Filtres par défauts
+let filtres = {
+    affichage: 'sum'
+}
+
+// Options CodeMirror
+let codeMirrorOptions = {
+    lineNumbers: true,
+    mode: "htmlmixed",
+    theme: "nord"
+}
+
+// CodeMirror sur l'input Modèle(s)
+var arrayModelesInput = CodeMirror.fromTextArea(get('#arrayModelesInput'), codeMirrorOptions);
+
+// Live check sur l'input Modèle(s)
+arrayModelesInput.on("change", (cm) => { 
+    arrayModeles = cm.getValue();
+    check(arrayListes, arrayModeles, filtres);
+})
+
+// CodeMirror sur l'input Liste(s)
+var arrayListesInput = CodeMirror.fromTextArea(get('#arrayListesInput'), codeMirrorOptions);
+
+// Live check sur l'input Liste(s)
+arrayListesInput.on("change", (cm) => { 
+    arrayListes = cm.getValue();
+    check(arrayListes, arrayModeles, filtres);
+})
 
 /**
  * Vérifie tous les arrays de l'object avec l'array modèle et retourne les arrays qui correspondent.
- * @param {String} object - Object de tous les arrays à vérifier (chaque arrays séparés par un \n et chaque valeur par une virgule)
- * @param {String} compare - Array modèle (chaque valeur séparés par une virgule)
+ * @param {String} listes - String de toutes les listes tapées 
+ * (chaque liste séparée par un (\r)\n et chaque valeur par une virgule)
+ * @param {String} modeles - String de tous les modèles tapés 
+ * (chaque modèle séparé par un (\r)\n et chaque valeur par une virgule)
  * @returns {Array}
  */
-function check (object, compare) {
-    // Manipulation des valeurs
-
-    /**
-     * Séparation du modèle par virgules.
-     * Escape la valeur avec trim().
+function check (listes, modeles, filters) {
+    /** Explications
+     * 1 - Séparation à chaque retour à la ligne.
+     * 2 - Création d'un nouvel Array.
+     * 3.1 - Pour chaque ligne, 
+     * 3.2 - Séparer à chaque virgule;
+     * 3.3 - .trim() chaque valeur et l'ajouter dans l'Array final.
      */
-    let compareParsed = {};
-    for (const [key, value] of Object.entries(compare)) {
-        // console.log(key, value);
-        compareParsed[key] = value.split(',').map(function (item) {
-            return item.trim();
-        });
-    }
-    /* Debug */ // console.log(`[check.compareParsed] Array modèle découpé par virugles\n`, compareParsed);
 
-    // Séparation de l'input à chaque virgule
-    let objectArraysNotParsed = object.split(/\r?\n/g);
-    /* Debug */ // console.log(`[check.objectArraysNotParsed] Arrays découpés par lignes\n`, objectArraysNotParsed);
-
-    /**
-     * Création d'un objet temporaire (objectParsed)
-     * Pour chaque array, l'ajouter dans l'objet temp.
-     * Séparer le string de chaque array par une virgule.
-     * Escape les valeurs avec trim().
-     * Ajouter ces nouvelles valeurs dans l'objet temporaire (objectParsed)
-     */
-    let objectParsed = [];
-    objectArraysNotParsed.forEach((arrayVal, arrayKey) => {
-        objectParsed.push(arrayVal.split(','))
-        objectParsed[arrayKey].forEach((val, key) => objectParsed[arrayKey][key] = val.trim())
+    // Liste(s) //
+    /* 1 */ let listesArrayOfString = listes.split(/\r?\n/g);
+    /* 2 */ let listesParsed = [];
+    /* 3.1 */ listesArrayOfString.forEach((arrayVal, arrayKey) => {
+    /* 3.2 */    listesParsed.push(arrayVal.split(','))
+    /* 3.3 */    listesParsed[arrayKey].forEach((val, key) => listesParsed[arrayKey][key] = val.trim())
     });
-    /* Debug */ // console.log(`[check.objectParsed] Object d'arrays découpés par lignes et par virgules\n`, objectParsed);
 
-    // Vérification w/(objectParsed, compareParsed)
-    let results = get('#return');
+    // Modèle(s) //
+    /* 1 */ let modelesArrayOfString = modeles.split(/\r?\n/g);
+    /* 2 */ let modelesParsed = [];
+    /* 3.1 */ modelesArrayOfString.forEach((arrayVal, arrayKey) => {
+    /* 3.2 */    modelesParsed.push(arrayVal.split(','))
+    /* 3.3 */    modelesParsed[arrayKey].forEach((val, key) => modelesParsed[arrayKey][key] = val.trim())
+    });
+
+    // Retour //
+    // Initialisation des valeurs de retour.
+    let returnDiv = get('#return'), output;
+
+    // Vérification des filtres.
+    switch (filters.affichage) {
+        // Addition des résultats entre modèles.
+        case 'sum':
+            output = orderBySum(listesParsed, modelesParsed);
+            break;
+    }
+
+    // Retour du résultat.     
+    returnDiv.innerHTML = output;
+}
+
+/**
+ * Retourne les résultats du check en vérifiant toutes les listes
+ * avec tous les modèles et en additionnant les résultats.
+ * @param {Array} listes - Toutes les listes (en array de chaques)
+ * @param {Array} modeles - Tous les modèles (en dict {1: []...})
+ */
+function orderBySum (listes, modeles) {
     let output = ``;
 
-    objectParsed.forEach((thisArray, thisArrayKey) => {
-        let outputByTemplate = ``;
+    
 
-        thisArray.forEach((arrayValue, key) => {
-            for (const [compareKey, compareKeyValue] of Object.entries(compareParsed)) {
-                // console.log(`Key de l'array à comparer: ${compareKey};\n Ses valeurs:`, compareKeyValue);
 
-                let thisArraySameCount = 0;
-
-                compareKeyValue.forEach((compareValue, key) => {
-                    if (arrayValue == compareValue && arrayValue != "") {
-                        thisArraySameCount++;
-                        outputByTemplate += `<p>${thisArray} -> ${thisArraySameCount} count dans ${compareKey}</p>`;
+    // Pour chaque listes, récupérer les lignes
+    for (const [listeArrayKey, listeArrayValues] of Object.entries(listes)) {
+        let thisListCount = 0;
+        // Pour chaque valeurs d'une liste
+        listeArrayValues.forEach((listeValue, listeKeyofValue) => {
+            // Pour chaque modèles, récupérer les lignes
+            for (const [modeleArrayKey, modeleArrayValues] of Object.entries(modeles)) {
+                // Pour chaque valeurs d'un modèle
+                modeleArrayValues.forEach((modeleValue, modeleKeyofValue) => {
+                    // Si la valeur du modèle est égale à une valeur
+                    // de la liste, alors ajouter 1 à l'indicateur de la liste
+                    if (listeValue == modeleValue && listeValue != "") {
+                        thisListCount++;
                     }
                 })
             }
         })
-        if (outputByTemplate != "") {
-            output = outputByTemplate;
-        }
-    })
 
-    results.innerHTML = output;
+        if (thisListCount > 0)
+            output += `
+                <p><code class="code">${parseInt(listeArrayKey) + 1}</code> contient <b>${thisListCount}</b> correspondances pour tous les modèles.</p>
+            `;
+    }
+
+    return output;
 }
 
-// Génération de modèles
-const templateGenerated = get('#templateGenerated');
-get('#addTemplate').addEventListener('click', () => {
-    let count = 1;
+////////////
+// Bulma CSS
+var rootEl = document.documentElement;
+var $modals = getAll('.modal');
+var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
 
-    // Tant que l'ID choisie est déjà attribuée, changer l'ID
-    while(document.contains(get(`#template-${count}`))) count++;
-
-    let output = document.createElement('div');
-        output.className = "field has-addons has-addons-centered";
-        output.innerHTML = `
-            <p class="control">
-                <button class="button is-static">${count}</button>
-            </p>
-            <p class="control is-expanded">
-                <input class="input isATemplateInput" type="text" placeholder="exemple: 1,5,2,4,3"
-                    id="template-${count}"
-                />
-            </p>
-            <p class="control">
-                <button class="button is-danger" onClick="removeTemplate('#template-${count}')">${deleteSvg}</button>
-            </p>
-        `;
-    templateGenerated.appendChild(output);
-    refreshTemplatesListener();
-});
-
-/**
- * Enlève un modèle à partir de l'ID
- * @param {String} id - ID du modèle
- */
-function removeTemplate (id) {
-    var el = get(id);
-    var parentEl =  el.parentNode;
-    var divEl = parentEl.parentNode;
-
-    divEl.remove();
-    refreshTemplatesListener();
-}
-
-function refreshTemplatesListener () {
-    // Réinitialisation des modèles
-    arrayToCompare = {};
-    
-    // ForEach the inputs
-    let allInputs = getAll('.isATemplateInput');
-    allInputs.forEach(function (el) {
-        // Récupérations des IDs
-        let inputKeyFromID = el.id.replace('template-', '');
-
-        // Refresh les valeurs
-        arrayToCompare[inputKeyFromID] = el.value;
-
-        // Refresh même onInput
-        el.addEventListener('input', function (el) {
-            arrayToCompare[inputKeyFromID] = this.value;
-            check(objectOfArrays, arrayToCompare);
-
-            /* Debug */ // console.log(`[refreshTemplates.input]\n`, arrayToCompare);
+if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+            closeModals();
         });
     });
-
-    console.log(`[refreshTemplates.output]\n`, arrayToCompare);
 }
+
+function openModal(target) {
+    var $target = document.getElementById(target);
+    rootEl.classList.add('is-clipped');
+    $target.classList.add('is-active');
+}
+
+function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+        $el.classList.remove('is-active');
+    });
+}
+
+document.addEventListener('keydown', function (event) {
+    var e = event || window.event;
+    (e.keyCode === 27) && closeModals();
+});
+
